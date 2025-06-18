f0d14248dc83b285f7eaef789444a09fd00a99b2
HarshilShah1804 <hkshah1978@gmail.com>
2025-06-18 14:07:11 +0530

    fix: update image input node for nultiple window rendering

--- src/nodes/ImageInputNode.cpp
+++ src/nodes/ImageInputNode.cpp
@@ -1,30 +1,91 @@
-// ImageInputNode.cpp
 #include "ImageInputNode.h"
+
 #include <imgui.h>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <GL/gl.h>
+#include <iostream>
 
 ImageInputNode::ImageInputNode() {
-    position = ImVec2(100, 100);
+    filepath = "";
+    image = cv::Mat();
+    textureID = 0;
+    textureValid = false;
+    memset(inputBuffer, 0, sizeof(inputBuffer));
+    name = "Image Input Node";
+}
+
+ImageInputNode::~ImageInputNode() {
+    if (textureID != 0) {
+        glDeleteTextures(1, &textureID);
+        textureID = 0;
+    }
+}
+
+void ImageInputNode::process() {
+    // No processing for input node
+}
+
+std::string ImageInputNode::getName() const {
+    return name;
+}
+
+cv::Mat ImageInputNode::getImage() const {
+    return image;
+}
+
+void ImageInputNode::loadImage(const std::string& path) {
+    filepath = path;
+    image = cv::imread(path);
+
+    if (image.empty()) {
+        std::cerr << "Failed to load image from: " << path << std::endl;
+        return;
+    }
+
+    // Delete previous texture if any
+    if (textureID != 0) {
+        glDeleteTextures(1, &textureID);
+        textureID = 0;
+    }
+
+    // Convert to RGBA
+    cv::Mat displayImage;
+    if (image.channels() == 1)
+        cv::cvtColor(image, displayImage, cv::COLOR_GRAY2RGBA);
+    else if (image.channels() == 3)
+        cv::cvtColor(image, displayImage, cv::COLOR_BGR2RGBA);
+    else if (image.channels() == 4)
+        displayImage = image.clone();
+    else
+        return;
+
+    cv::flip(displayImage, displayImage, 0);  // Flip for OpenGL
+
+    // Upload to texture
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, displayImage.cols, displayImage.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, displayImage.ptr());
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    textureValid = true;
 }
 
 void ImageInputNode::renderUI() {
-    ImGui::Begin(getName().c_str());
+    ImGui::Begin(("Image Input Node " + std::to_string(id)).c_str());
 
-    ImGui::InputText("Path", imagePathBuffer, IM_ARRAYSIZE(imagePathBuffer));
-    if (ImGui::Button("Load Image")) {
-        image = cv::imread(imagePathBuffer);
-        loaded = !image.empty();
-        if (loaded) updateTexture(image);
+    ImGui::InputText("File Path", inputBuffer, IM_ARRAYSIZE(inputBuffer));
+    if (ImGui::Button("Load")) {
+        filepath = std::string(inputBuffer);
+        loadImage(filepath);
     }
 
-    if (loaded && textureID != 0) {
+    if (textureValid && textureID != 0) {
         ImGui::Text("Preview:");
-        ImGui::Image((void*)(intptr_t)textureID, ImVec2(150, 150));
+        ImGui::Image((ImTextureID)(intptr_t)textureID, ImVec2(128, 128));
     }
 
     ImGui::End();
-}
-
-
-void ImageInputNode::process() {
-    if (loaded) updateTexture(image);
 }
--- src/nodes/ImageInputNode.h
+++ src/nodes/ImageInputNode.h
@@ -1,18 +1,24 @@
 #pragma once
 #include "Node.h"
 #include <opencv2/opencv.hpp>
+#include <GL/gl.h>
+#include <string>
 
 class ImageInputNode : public Node {
 public:
     ImageInputNode();
+    ~ImageInputNode();
+
     void renderUI() override;
     void process() override;
-    std::string getName() const override { return "Image Input"; }
-    const cv::Mat& getImage() const { return image; }
-// In ImageInputNode.h
-private:
-    char imagePathBuffer[256] = "";  // <-- char buffer
-    cv::Mat image;
-    bool loaded = false;
+    std::string getName() const override;
+    cv::Mat getImage() const override;
+    void loadImage(const std::string& path);
 
+private:
+    std::string filepath;
+    cv::Mat image;
+    GLuint textureID;
+    bool textureValid;
+    char inputBuffer[512];
 };
