47a6f9db89e4f89cccbb2b116093da27c994671c
HarshilShah1804 <hkshah1978@gmail.com>
2025-06-20 13:51:32 +0530

    rem: remove extra dependencies

--- src/nodes/Node.cpp
+++ src/nodes/Node.cpp
@@ -1,21 +1,4 @@
-// Node.cpp
 #include "Node.h"
+#include <imgui.h>
 
-void Node::updateTexture(const cv::Mat& image) {
-    if (image.empty()) return;
-
-    if (textureID == 0) {
-        glGenTextures(1, &textureID);
-    }
-
-    cv::Mat rgba;
-    if (image.channels() == 1)
-        cv::cvtColor(image, rgba, cv::COLOR_GRAY2RGBA);
-    else
-        cv::cvtColor(image, rgba, cv::COLOR_BGR2RGBA);
-
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, rgba.cols, rgba.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, rgba.ptr());
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-}
+Node::~Node() = default;  // This is essential for virtual destructors in abstract classes
--- src/nodes/Node.h
+++ src/nodes/Node.h
@@ -1,19 +1,19 @@
-// Node.h
 #pragma once
-#include <opencv2/opencv.hpp>
+#include <string>
+#include <opencv2/core.hpp>
 #include <imgui.h>
-#include <GL/gl.h>
 
 class Node {
 public:
+    virtual ~Node();  // Declare, but not define here
+
     virtual void renderUI() = 0;
     virtual void process() = 0;
     virtual std::string getName() const = 0;
-
-    // Optional: use this for future dragging or layout
-    ImVec2 position = ImVec2(100, 100);
+    virtual cv::Mat getImage() const = 0;
+    int id = -1;
 
 protected:
-    GLuint textureID = 0;
-    void updateTexture(const cv::Mat& image);
+    std::string name;
+    ImVec2 position = ImVec2(0, 0);
 };
